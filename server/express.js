12bc56172f3b6d320f275a9826fa365131e9fafc
donchase <donchase@gmail.com>
2021-02-23 14:31:49 -0500

    Setting up Front End

--- server/express.js
+++ server/express.js
@@ -7,8 +7,16 @@ import helmet from 'helmet'
 import Template from './../template'
 import userRoutes from './routes/user.routes'
 import authRoutes from './routes/auth.routes'
+import path from 'path'
+
+// next line is for development only: Comment out for production
+import devBundle from './devBundle'
 
 const app = express()
+const CURRENT_WORKING_DIR = process.cwd()
+
+// next line is for development only: Comment out for production
+devBundle.compile(app)
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -18,6 +26,7 @@ app.use(helmet())
 app.use(cors())
 app.use('/', userRoutes)
 app.use('/', authRoutes)
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
 app.get('/', (req, res) => {
     res.status(200).send(Template())
