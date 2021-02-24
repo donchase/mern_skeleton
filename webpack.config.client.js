c09d4bf4313f51c9ce9db1d75438e9128d9c41fb
donchase <donchase@gmail.com>
2021-02-24 14:10:49 -0500

    Front End

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -10,7 +10,7 @@ const config = {
         'webpack-hot-middleware/client?reload=true', path.join(CURRENT_WORKING_DIR, 'client/main.js')
     ],
     output: {
-        path: path.join(CURRENT_WORKING_DIR , '/dist'),
+        path: path.join(CURRENT_WORKING_DIR, '/dist'),
         filename: 'bundle.js',
         publicPath: '/dist/'
     },
@@ -20,6 +20,10 @@ const config = {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
+            },
+            {
+                test: /\.(ttf|eot|svg|gif|jpg|png)(\?[\s\S]+)?$/,
+                use: 'file-loader'
             }
         ]
     },
